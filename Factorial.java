ea5cf4aad9326f1c66037bdf9de7c83d1bd2cd48
Niket Mishra <157272356+Niket-mishra@users.noreply.github.com>
2024-01-29 20:34:36 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,22 @@
+// Java Program to Find Factorial of a Number
+
+import java.io.*;
+import java.util.*;
+
+static class Factorial {
+
+ int factorial(int n)
+	{
+		if (n == 1)
+			return 1;
+else{
+		return n * factorial(n - 1);
+	}
+
+	public static void main(String[] args)
+	{
+		int ans = factorial(5);
+
+		System.out.println("Factorial of 5 is :" + ans);
+	}
+}
